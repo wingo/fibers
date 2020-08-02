81678c43b69f1ad9792e84f2176380dbfd91fdb9
Aleix Conchillo Flaque <aconchillo@gmail.com>
2020-08-01 17:47:59 -0700

    libevent: allow event loop to block until events occur
    * fibers/libevent.scm (make-wake-pipe): creates wake pipes so fibers can wake
    up the event loop.
    
    * fibers/libevent.scm (pump-libevt-guardian): set libevent guardia for
    GC. without this it was not allowing to cancel in the REPL and libevent was
    kept running.
    
    * libevent.c (run_event_loop): run the events only once since they will be
    registered again, except wake events which persist.
    
    * libevent.c (scm_primitive_event_loop): call run event loop and ignore wake
    events.

--- fibers/libevent.scm
+++ fibers/libevent.scm
@@ -11,6 +11,7 @@
               libevt-destroy
               libevt?
               libevt-add!
+              libevt-wake!
               libevt
               EVREAD EVWRITE EVERR))
 
@@ -26,14 +27,27 @@
 
 (define EVERR (logior EVREAD EVWRITE))
 
+(define (make-wake-pipe)
+  (define (set-nonblocking! port)
+    (fcntl port F_SETFL (logior O_NONBLOCK (fcntl port F_GETFL))))
+  (let ((pair (pipe)))
+    (match pair
+      ((read-pipe . write-pipe)
+       (setvbuf write-pipe 'none)
+       (set-nonblocking! read-pipe)
+       (set-nonblocking! write-pipe)
+       (values read-pipe write-pipe)))))
+
 (define-record-type <libevt>
-  (make-libevt ls eventsv maxevents state)
+  (make-libevt ls eventsv maxevents state wake-read-pipe wake-write-pipe)
   libevt?
   (ls libevt-ls set-libevt-ls!)
   (eventsv libevt-eventsv set-libevt-eventsv!)
   (maxevents libevt-maxevents set-libevt-maxevents!)
   ;; atomic box of either 'waiting, 'not-waiting or 'dead
-  (state libevt-state))
+  (state libevt-state)
+  (wake-read-pipe libevt-wake-read-pipe)
+  (wake-write-pipe libevt-wake-write-pipe))
 
 (define-syntax fd-offset
   (lambda (x)
@@ -52,24 +66,41 @@
 (define (pump-libevt-guardian)
   (let ((libevt (libevent-guardian)))
     (when libevt
-      (pump-libevt-guardian))
-    (add-hook! after-gc-hook pump-libevt-guardian)))
+      (libevt-destroy libevt)
+      (pump-libevt-guardian))))
+(add-hook! after-gc-hook pump-libevt-guardian)
 
 (define* (libevt-create #:key (maxevents 8))
-  (let* ((state (make-atomic-box 'not-waiting))
-         (eventsv (make-bytevector (fd-offset (or maxevents 8))))
-         (libevt (make-libevt (primitive-create-event-base eventsv)
-                              eventsv maxevents state)))
-    (libevent-guardian libevt)
-    libevt))
+  (call-with-values (lambda () (make-wake-pipe))
+    (lambda (read-pipe write-pipe)
+      (let* ((state (make-atomic-box 'not-waiting))
+             (eventsv (make-bytevector (fd-offset (or maxevents 8))))
+             (libevt (make-libevt (primitive-create-event-base eventsv)
+                                  eventsv maxevents state read-pipe write-pipe)))
+        (libevent-guardian libevt)
+        (libevt-add! libevt (fileno read-pipe) (logior EVREAD EVPERSIST))
+        libevt))))
 
 (define (libevt-destroy libevt)
   (atomic-box-set! (libevt-state libevt) 'dead)
   (when (libevt-ls libevt)
+    (close-port (libevt-wake-read-pipe libevt))
+    ;; FIXME: ignore errors flushing output
+    (close-port (libevt-wake-write-pipe libevt))
     (set-libevt-ls! libevt '())))
 
 (define* (libevt-add! libevt fd events)
   (primitive-add-event (libevt-ls libevt) fd events))
+
+(define (libevt-wake! libevt)
+  (match (atomic-box-ref (libevt-state libevt))
+    ;; It is always correct to wake an epoll via the pipe.  However we
+    ;; can avoid it if the epoll is guaranteed to see that the
+    ;; runqueue is not empty before it goes to poll next time.
+    ('waiting
+     (primitive-event-wake (fileno (libevt-wake-write-pipe libevt))))
+    ('not-waiting #t)
+    ('dead (error "libevt instance is dead"))))
 
 (define (libevt-default-folder fd events seed)
   (acons fd events seed))
@@ -87,10 +118,13 @@
          ((< expiry now) 0)
          (else (- expiry now)))))))
   (let* ((maxevents (libevt-maxevents libevt))
-         (eventsv (libevt-eventsv libevt)))
+         (eventsv (libevt-eventsv libevt))
+         (write-pipe-fd (fileno (libevt-wake-write-pipe libevt)))
+         (read-pipe-fd (fileno (libevt-wake-read-pipe libevt))))
     (atomic-box-set! (libevt-state libevt) 'waiting)
     (let* ((timeout (expiry->timeout (update-expiry expiry)))
-           (n (primitive-event-loop (libevt-ls libevt) timeout)))
+           (n (primitive-event-loop (libevt-ls libevt)
+                                    write-pipe-fd read-pipe-fd timeout)))
       (let lp ((seed seed) (i 0))
         (if (< i n)
             (let ((fd (bytevector-s32-native-ref eventsv (fd-offset i)))
