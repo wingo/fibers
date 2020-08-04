1d141107eb385a2fc4ae7bde1db63ade6de9fe0f
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2020-08-04 11:44:41 -0700

    add missing copyright notices

--- clock_nanosleep.c
+++ clock_nanosleep.c
@@ -1,4 +1,20 @@
-#ifdef __APPLE__
+/* Copyright (C) 2020 Aleix Conchillo Flaqué <aconchillo@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
 
 #include "clock_nanosleep.h"
 
@@ -12,5 +28,3 @@ clock_nanosleep(clockid_t id, int flags, const struct timespec *ts,
   int ret = nanosleep(ts, ots);
   return ret ? errno : 0;
 }
-
-#endif // __APPLE__
--- clock_nanosleep.h
+++ clock_nanosleep.h
@@ -1,4 +1,20 @@
-#ifdef __APPLE__
+/* Copyright (C) 2020 Aleix Conchillo Flaqué <aconchillo@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
 
 #ifndef CLOCK_NANOSLEEP_H
 #define CLOCK_NANOSLEEP_H
@@ -13,4 +29,3 @@ int clock_nanosleep (clockid_t id, int flags, const struct timespec *ts,
                      struct timespec *ots);
 
 #endif // CLOCK_NANOSLEEP_H
-#endif // __APPLE__
