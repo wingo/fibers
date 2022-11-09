wingo/fibers

commit c25dcb9cc4b5b977474ffe555b40ce2f1d0d1edc

'schedule-task-when-fd-active' always adds FD finalizer.
Previously, if we repeatedly did, say:

  socket() = 12;
  schedule-task-when-fd-active(12);
  close(12);

then 'fd-waiters' would only be cleared the first time.  On subsequent
iterations, the 'fd-waiters' list would not be cleared, thus this
condition would be verified:

  (and active-events
       (= (logand events active-events) events))

This, in turn, means that the FD would not be re-added to the epoll set,
and events on that FD would never be served.

* fibers/scheduler.scm (schedule-task-when-fd-active)[fd-finalizer]: New
procedure.
Call 'add-fdes-finalizer!' before the first 'epoll-add*!' call.
Use 'fd-finalizer' instead of the local 'finalize-fd' procedure.
* tests/ports.scm: New file.
* Makefile.am (TESTS): Add it.

fibers/scheduler.scm
@@ -320,6 +320,20 @@ value.  Return zero values."
   "Arrange for @var{sched} to schedule @var{task} when the file
 descriptor @var{fd} becomes active with any of the given @var{events},
 expressed as an epoll bitfield."
+  (define (fd-finalizer fd-waiters)
+    (lambda (fd)
+      ;; When a file port is closed, clear out the list of
+      ;; waiting tasks so that when/if this FD is re-used, we
+      ;; don't resume stale tasks.  Note that we don't need to
+      ;; remove the FD from the epoll set, as the kernel manages
+      ;; that for us.
+      ;;
+      ;; FIXME: Is there a way to wake all tasks in a thread-safe
+      ;; way?  Note that this function may be invoked from a
+      ;; finalizer thread.
+      (set-cdr! fd-waiters '())
+      (set-car! fd-waiters #f)))
+
   (let ((fd-waiters (hashv-ref (scheduler-fd-waiters sched) fd)))
     (match fd-waiters
       ((active-events . waiters)
@@ -328,24 +342,13 @@ expressed as an epoll bitfield."
                     (= (logand events active-events) events))
          (let ((active-events (logior events (or active-events 0))))
            (set-car! fd-waiters active-events)
+           (add-fdes-finalizer! fd (fd-finalizer fd-waiters))
            (epoll-add*! (scheduler-epfd sched) fd
                         (logior active-events EPOLLONESHOT)))))
       (#f
        (let ((fd-waiters (list events (cons events task))))
-         (define (finalize-fd fd)
-           ;; When a file port is closed, clear out the list of
-           ;; waiting tasks so that when/if this FD is re-used, we
-           ;; don't resume stale tasks.  Note that we don't need to
-           ;; remove the FD from the epoll set, as the kernel manages
-           ;; that for us.
-           ;;
-           ;; FIXME: Is there a way to wake all tasks in a thread-safe
-           ;; way?  Note that this function may be invoked from a
-           ;; finalizer thread.
-           (set-cdr! fd-waiters '())
-           (set-car! fd-waiters #f))
          (hashv-set! (scheduler-fd-waiters sched) fd fd-waiters)
-         (add-fdes-finalizer! fd finalize-fd)
+         (add-fdes-finalizer! fd (fd-finalizer fd-waiters))
          (epoll-add*! (scheduler-epfd sched) fd
                       (logior events EPOLLONESHOT)))))))
 

Makefile.am
@@ -68,6 +68,7 @@ TESTS = \
 	tests/channels.scm \
 	tests/foreign.scm \
 	tests/parameters.scm \
+        tests/ports.scm \
 	tests/preemption.scm \
 	tests/speedup.scm
 TESTS_ENVIRONMENT=top_srcdir="$(abs_top_srcdir)" ./env $(GUILE) -s

tests/ports.scm
@@ -1,0 +1,116 @@
+;; Fibers: cooperative, event-driven user-space threads.
+
+;;;; Copyright (C) 2022 Ludovic Courtès <ludo@gnu.org>
+;;;;
+;;;; This library is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU Lesser General Public
+;;;; License as published by the Free Software Foundation; either
+;;;; version 3 of the License, or (at your option) any later version.
+;;;;
+;;;; This library is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with this library; if not, write to the Free Software
+;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+;;;;
+
+(define-module (tests ports)
+  #:use-module (fibers)
+  #:use-module (fibers channels)
+  #:use-module (fibers scheduler)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 rdelim)
+  #:use-module ((ice-9 ports internal)
+                #:select (port-read-wait-fd)))
+
+(define* (bind* sock address)
+  ;; Like 'bind', but retry upon EADDRINUSE.
+  (let loop ((n 5))
+    (define result
+      (catch 'system-error
+        (lambda ()
+          (bind sock address))
+        (lambda args
+          (if (and (= EADDRINUSE (system-error-errno args))
+                   (> n 0))
+              'in-use
+              (apply throw args)))))
+
+    (when (eq? result 'in-use)
+      (sleep 1)
+      (loop (- n 1)))))
+
+
+(run-fibers
+ (lambda ()
+   (let* ((address (make-socket-address AF_INET INADDR_LOOPBACK 5556))
+          (notification (make-channel)))
+     (spawn-fiber
+      (lambda ()
+        ;; The server.
+        (let loop ((n 5))
+          ;; Create a socket; the underlying file descriptor has the same
+          ;; value for each iteration.  What we're checking here is that the
+          ;; waiter list and epoll set are properly updated when a file
+          ;; descriptor with the same value is reused, allowing the eventual
+          ;; 'accept' call to succeed.
+          (let ((sock (socket AF_INET SOCK_STREAM 0)))
+            (pk 'listening-socket sock)
+            (setsockopt sock SOL_SOCKET SO_REUSEADDR 1)
+            (bind* sock address)
+            (listen sock 1)
+            (fcntl sock F_SETFL
+                   (logior O_NONBLOCK (fcntl sock F_GETFL)))
+
+            (if (zero? n)
+                (begin
+                  ;; Let's go.
+                  (put-message notification 'ready!)
+                  (match (pk 'accepted-connection (accept sock))
+                    ((connection . _)
+                     (fcntl connection F_SETFL
+                            (logior O_NONBLOCK (fcntl connection F_GETFL)))
+                     (display (pk 'received (read-line connection)) connection)
+                     (newline connection)
+                     (close-port connection))))
+                (begin
+                  ;; Spawn a fiber and have it wait on SOCK.  Then
+                  ;; immediately close SOCK.  The next loop iteration creates
+                  ;; a new file descriptor with the same value.
+                  (spawn-fiber
+                   (lambda ()
+                     (suspend-current-task
+                      (lambda (sched k)
+                        (schedule-task-when-fd-readable sched
+                                                        (port-read-wait-fd sock)
+                                                        k)
+                        (close-port sock)))))
+                  (loop (- n 1))))))))
+
+     (spawn-fiber
+      (lambda ()
+        ;; Watchdog: bail out after some time has passed.
+        (sleep 30)
+        (display "timeout!\n" (current-error-port))
+        (primitive-_exit 1)))
+
+     ;; Wait for the server to be ready.
+     (match (get-message notification)
+       ('ready!
+        ;; Connect, send a message, and receive its echo.
+        (let ((sock (socket AF_INET SOCK_STREAM 0)))
+          (fcntl sock F_SETFL
+                 (logior O_NONBLOCK (fcntl sock F_GETFL)))
+          (connect sock address)
+          (pk 'connected address)
+          (display "hello!\n" sock)
+          (match (pk 'echo (read-line sock))
+            ("hello!"
+             (close-port sock)
+             (display "success\n" (current-error-port)))))))))
+
+ #:hz 0                                           ;cooperative scheduling
+ #:parallelism 1)                                 ;single-threaded

commit a9c2dbb72b3dd1919e5ca615f38ec719dd424893

scheduler: Do not re-add FD finalisers on FDs that already have one.
Fixes #65.

Commit c25dcb9cc4b5b977474ffe555b40ce2f1d0d1edc introduced a memory
leak, whereby 'schedule-tasks-for-active-fd' would keep adding fdes
finalizers on actively-used file descriptors, for example when calling
'accept' several times on the same file descriptor.

This commit fixes it by allowing 'schedule-tasks-for-active-fd' to
distinguish between "finalized file descriptor" and "file descriptor
without waiters but with a finalizer already".

* fibers/scheduler.scm (schedule-tasks-for-active-fd): Set the car of
EVENTS+WAITERS to 0, not #f.
(schedule-task-when-fd-active): Treat the cases where FD-WAITERS = #f
and FD-WAITERS = '(#f) in the same way.  In the other case, assume
ACTIVE-EVENTS is an integer.

fibers/scheduler.scm
@@ -182,8 +182,10 @@ remote kernel thread."
     (#f (warn "scheduler for unknown fd" fd))
     ((and events+waiters (active-events . waiters))
      ;; First, clear the active status, as the EPOLLONESHOT has
-     ;; deactivated our entry in the epoll set.
-     (set-car! events+waiters #f)
+     ;; deactivated our entry in the epoll set.  Set the car to 0, not #f, so
+     ;; that 'schedule-tasks-for-active-fd' doesn't end up re-adding a
+     ;; finalizer on FD.
+     (set-car! events+waiters 0)
      (set-cdr! events+waiters '())
      (unless (zero? (logand revents (logior EPOLLHUP EPOLLERR)))
        (hashv-remove! (scheduler-fd-waiters sched) fd))
@@ -336,21 +338,19 @@ expressed as an epoll bitfield."
 
   (let ((fd-waiters (hashv-ref (scheduler-fd-waiters sched) fd)))
     (match fd-waiters
-      ((active-events . waiters)
-       (set-cdr! fd-waiters (acons events task waiters))
-       (unless (and active-events
-                    (= (logand events active-events) events))
-         (let ((active-events (logior events (or active-events 0))))
-           (set-car! fd-waiters active-events)
-           (add-fdes-finalizer! fd (fd-finalizer fd-waiters))
-           (epoll-add*! (scheduler-epfd sched) fd
-                        (logior active-events EPOLLONESHOT)))))
-      (#f
+      ((or #f (#f))                               ;FD is new or was finalized
        (let ((fd-waiters (list events (cons events task))))
          (hashv-set! (scheduler-fd-waiters sched) fd fd-waiters)
          (add-fdes-finalizer! fd (fd-finalizer fd-waiters))
          (epoll-add*! (scheduler-epfd sched) fd
-                      (logior events EPOLLONESHOT)))))))
+                      (logior events EPOLLONESHOT))))
+      ((active-events . waiters)
+       (set-cdr! fd-waiters (acons events task waiters))
+       (unless (= (logand events active-events) events)
+         (let ((active-events (logior events active-events)))
+           (set-car! fd-waiters active-events)
+           (epoll-add*! (scheduler-epfd sched) fd
+                        (logior active-events EPOLLONESHOT))))))))
 
 (define (schedule-task-when-fd-readable sched fd task)
   "Arrange to schedule @var{task} on @var{sched} when the file