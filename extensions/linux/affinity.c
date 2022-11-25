a9b0f37f503f175f9a7a35eb69ca4d544569f991
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-11-25 12:26:13 -0800

    re-organize extentions and platform files into folders

---
+++ extensions/linux/affinity.c
@@ -0,0 +1,2 @@
+// Guile already provides setaffinity/getaffinity on Linux, so this is just a
+// placeholder for "make distcheck".
---
+++ extensions/linux/clock-nanosleep.c
@@ -0,0 +1,2 @@
+// Linux already provides the implementation we want for clock_nanosleep, so
+// this is just a placeholder for "make distcheck".
