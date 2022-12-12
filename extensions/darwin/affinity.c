74db78cbdb3f27a8506eac22246df32cee8fe483
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2022-12-11 16:40:22 -0800

    affinity: remove copyright lines

--- extensions/darwin/affinity.c
+++ extensions/darwin/affinity.c
@@ -1,6 +1,4 @@
-/* Copyright (C) 2020 Abdulrahman Semrie <hsamireh@gmail.com>
- * Copyright (C) 2020-2022 Aleix Conchillo Flaqué <aconchillo@gmail.com>
- *
+/*
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 3 of
